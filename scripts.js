9229256349c1bddbe7eb1a88e8d06cd07d4bef27
Shane Jeffers <shanejeffers@mac.com>
2013-04-05 09:25:48 -0400

    Removed duplicate script file

--- scripts.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$('.animated > li').hover(function(){
-		$(this).find('.container-4').stop().slideDown('fast');
-	},
-	function(){
-		$(this).find('.container-4').stop().slideUp('slow');
-	});
-});
-
